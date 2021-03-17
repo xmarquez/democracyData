b0314c1277d696d7ac30941efadcb71a9e691109
Xavier Marquez <marquexa@staff.vuw.ac.nz>
2021-03-17 22:28:01 +1300

    Revised tests for FH, Polity, and WGI to give the correct number of rows.

--- tests/testthat/test_polity.R
+++ tests/testthat/test_polity.R
@@ -17,9 +17,9 @@ test_that("Polity5 downloads correctly", {
   skip_on_cran()
   skip_on_travis()
   polity5 <- download_polity_annual(verbose = FALSE)
-  expect_equal(nrow(polity5), 17548)
+  expect_equal(nrow(polity5), 17574)
   expect_equal(nrow(polity5 %>% filter(duplicated(cyear))), 0)
-  expect_equal(max(polity5$year), 2018)
+  expect_equal(max(polity5$year), 2020)
   expect_false(any(is.na(polity5$GWn)))
 })
 
