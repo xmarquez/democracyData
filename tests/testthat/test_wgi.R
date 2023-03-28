057af72e6d815bb7911cb16459f1fce389e999df
Xavier Marquez <marquez.x@gmail.com>
2023-03-28 18:45:43 -0400

    Test change

--- tests/testthat/test_wgi.R
+++ tests/testthat/test_wgi.R
@@ -8,5 +8,5 @@ test_that("World Governance Indicators data downloads correctly", {
   wgi_democracy <- download_wgi_voice_and_accountability(verbose = FALSE)
   expect_equal(nrow(wgi_democracy), 4768)
   expect_equal(max(wgi_democracy$year), 2021)
-  expect_equal(sum(is.na(wgi_democracy$GWn)), 319)
+  expect_equal(sum(is.na(wgi_democracy$GWn)), 296)
 })
