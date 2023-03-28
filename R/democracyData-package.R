8262a7d2b44afae5e581513078dbfee37dc3852c
Xavier Marquez <marquez.x@gmail.com>
2023-03-28 14:17:11 -0400

    Documentation changes

---
+++ R/democracyData-package.R
@@ -0,0 +1,12 @@
+#' democracyData
+#'
+#'   This package archives a large number of democracy datasets and their
+#'   documentation, many of historical interest. It also contains a number of
+#'   functions to download and process the original source data and to determine
+#'   membership of a country in standard membership lists of the state system
+#'   (according to Gleditsch and Ward, Correlates of War, or Polity).
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
